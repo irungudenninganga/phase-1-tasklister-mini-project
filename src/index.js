61514cdbaa48374de5c21c4c05bb533449604d09
+dennisngangairungu <+>
2023-10-13 22:39:57 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let ulEl=document.getElementById('tasks')
+
+  let inputEl = document.querySelector('form')
+
+  let newTask=document.getElementById('new-task-description')
+  inputEl.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let liEl=document.createElement('li')
+    liEl.innerHTML=newTask.value
+    ulEl.appendChild(liEl)
+    newTask.value=''
+
+    let deleteBtn = document.createElement('button')
+    deleteBtn.innerHTML='delete'
+    liEl.appendChild(deleteBtn)
+    deleteBtn.style.marginLeft='4px'
+    deleteBtn.id='delete-btn'
+
+
+    deleteBtn.addEventListener('click', () => {
+      liEl.remove()
+    })
+  })
+
+
 });
+
+function deleteElement(){
+
+}
